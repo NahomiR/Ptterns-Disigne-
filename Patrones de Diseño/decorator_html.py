089446d28d1ffe01efd24e496c2017f6fe51ed7b
NahomiR <126628376+NahomiR@users.noreply.github.com>
2023-10-18 16:08:59 -0600

    Add files via upload

---
+++ Patrones de Diseño/decorator_html.py
@@ -0,0 +1,6 @@
+class HTMLNegativeDecorator(NegativeDecorator):
+    def format(self, data):
+        for section, values in data.items():
+            for key, value in values.items():
+                if value < 0:
+                    values[key] = f'<span style="color:red">{value}</span>'
